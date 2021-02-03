5d4fd1f214faa411a874591f3a62afa4be5e6dbb
Sven de Goede <72500744+Raz-rD@users.noreply.github.com>
2021-02-03 15:46:09 +0100

    Type: Addition. Change: Added day 1 files, including start of project and such. Working on spawn manager, Movement is done. Why: To keep up with version control, to show to teachers.

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 5f;
+    public Rigidbody rb;
+    private float sideBound = 10;
+    private float topBound = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+        //K
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement Code
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * Speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * -Speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(transform.right * -Speed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * Speed);
+        }
+
+        //Stop movement when near the edges of the screen
+        if (transform.position.z > topBound)
+        {
+            rb.velocity = new Vector3(0, 0, -1);
+        }
+        if (transform.position.z < -topBound)
+        {
+            rb.velocity = new Vector3(0, 0, 1);
+        }
+        if (transform.position.x > sideBound)
+        {
+            rb.velocity = new Vector3(-1, 0, 0);
+        }
+        if (transform.position.x < -sideBound)
+        {
+            rb.velocity = new Vector3(1, 0, 0);
+        }
+    }
+}
---
+++ Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject Collectible;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(methodName: "SpawnCollectible", time: startDelay, repeatRate: spawnInterval);
+    }
+
+    // Update is called once per frame
+    private float spawnRangeX = 10;
+    private float spawnPosZ = 10;
+    private float spawnTimer = 120;
+    void Update()
+    {
+
+
+
+
+
+
+    }
+
+    void SpawnCollectible()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(Collectible, spawnPos, transform.rotation);
+
+    }
+}
